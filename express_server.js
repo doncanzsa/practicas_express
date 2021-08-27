3cd932b0ce6e9522c9874ffb5ba6d7204d97f54f
doncans <doncanzsa@gmail.com>
2021-08-27 17:35:35 -0600

    agregamos lectura de json. y recibimos cuerpo y parametros mediante peticion

--- express_server.js
+++ express_server.js
@@ -1,13 +1,35 @@
 const express = require('express');
-
 const app = express();
 const port = 3001;
+
+app.use(express.json());  // recibiendo objetos de tipo JSON
 
 app.get('/', (req, res) => {
     res.send('Get Request Received');
 })
 
+app.get('/user', (req, res) => {
+    res.json({
+        userName: 'Didier',
+        lastName: 'Alvarez',
+    });
+})
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params)
+    res.json({
+
+        userName: 'Didier',
+        lastName: 'Alvarez',
+    });
+})
+
 app.post('/new', (req, res) => {
+    res.send('Post Request Received');
+})
+
+app.post('/user', (req, res) => {
+    console.log(req.body);
     res.send('Post Request Received');
 })
 
@@ -15,8 +37,9 @@ app.put('/update', (req, res) => {
     res.send('Put Request Received');
 })
 
-app.delete('/del', (req, res) => {
-    res.send('<h3>Delete Request Received</h3>');
+app.delete('/del/:id', (req, res) => {
+    console.log(req.params);
+    res.send(`El usuario con ID: ${req.params.id} ha sido eliminado`);
 })
 
 app.listen(port, () => {
