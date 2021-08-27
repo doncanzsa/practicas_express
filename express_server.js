242f41542ca4bfee06f20e4743a7490150a76c66
doncans <doncanzsa@gmail.com>
2021-08-27 16:59:35 -0600

    probando rutas y metodos

--- express_server.js
+++ express_server.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Welcome 123');
+    res.send('Get Request Received');
+})
+
+app.post('/new', (req, res) => {
+    res.send('Post Request Received');
+})
+
+app.put('/update', (req, res) => {
+    res.send('Put Request Received');
+})
+
+app.delete('/del', (req, res) => {
+    res.send('<h3>Delete Request Received</h3>');
 })
 
 app.listen(port, () => {
